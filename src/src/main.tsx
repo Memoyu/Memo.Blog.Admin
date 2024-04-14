90b7a805c63944d56744914c5f11f4c15f62cbbe
Memoyu <mmy6076@outlook.com>
2024-04-15 00:13:48 +0800

    🎨 格式化代码

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from '@redux/store.ts';
+import { store } from '@redux/store';
 import App from './App.tsx';
 import './index.css';
 
